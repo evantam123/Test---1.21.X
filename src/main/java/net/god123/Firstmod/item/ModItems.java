efc2c7d09b4273d3a14f3682394c270bafddfd62
96541 <96541@EVANS_PC>
2026-07-10 04:59:37 +0800

    heheheha

---
+++ src/main/java/net/god123/Firstmod/item/ModItems.java
@@ -0,0 +1,8 @@
+package net.god123.Firstmod.item;
+
+import net.god123.Firstmod.Firstmod;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items Items = DeferredRegister.createItems(Firstmod.MODID);
+}
